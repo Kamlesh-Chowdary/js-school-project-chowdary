e46f75b2fc080c8ef8d2d37ad772222c3df7661c
Kamlesh <115933182+Kamlesh-Chowdary@users.noreply.github.com>
2025-08-17 20:03:41 +0530

    feat: Event Timeline display functionality added

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,8 +1,7 @@
 import type { EventData } from "./interfaces/types.js";
 
 export async function fetchEvents(): Promise<EventData[]> {
-  const response = await fetch("events.json");
-  console.log(response)
+  const response = await fetch("data/events.json");
   if (!response.ok) throw new Error("Failed to load events");
   return response.json();
 }
