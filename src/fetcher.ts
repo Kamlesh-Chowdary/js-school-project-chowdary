d06e5dbdde043e74cf035a93a6b8960546679d3a
Kamlesh <115933182+Kamlesh-Chowdary@users.noreply.github.com>
2025-08-20 17:22:19 +0530

    React initial setup/installation done

--- src/fetcher.ts
+++
@@ -1,7 +0,0 @@
-import type { EventData } from "./interfaces/types.js";
-
-export async function fetchEvents(): Promise<EventData[]> {
-  const response = await fetch("data/events.json");
-  if (!response.ok) throw new Error("Failed to load events");
-  return response.json();
-}
